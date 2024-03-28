7ab3e49539deb0ff9eda5d5e0de558b72592c273
aragakerubo <aragakerubo@gmail.com>
2024-03-27 22:30:58 -0400

    9. Height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,7 +9,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_height, right_height;
 
-	if (tree == NULL)
+	if (!tree)
+		return (0);
+	if (!tree->left && !tree->right)
 		return (0);
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
